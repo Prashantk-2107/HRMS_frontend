596ab36807d93331e7c16fd7accb9b902356bd2f
Prashant <prashantkamboj.work@gmail.com>
2026-06-29 21:33:58 +0530

    Added logo

--- src/modules/auth/components/AuthHeader.jsx
+++ src/modules/auth/components/AuthHeader.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import IMG from "../../../assets/icons/WorkSphere.png"
 
 const AuthHeader = () => {
   return (
@@ -8,19 +9,7 @@ const AuthHeader = () => {
         to="/login"
         className="flex items-center gap-2 text-xl sm:text-2xl font-black text-indigo-600 select-none no-underline"
       >
-        <svg
-          className="text-indigo-600"
-          width="28"
-          height="28"
-          viewBox="0 0 32 32"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M16 2C8.26801 2 2 8.26801 2 16C2 23.732 8.26801 30 16 30C23.732 30 30 23.732 30 16C30 8.26801 23.732 2 16 2ZM14.5 22.5V9.5L22 16L14.5 22.5Z"
-            fill="currentColor"
-          />
-        </svg>
+        <img src={IMG} alt="W" className="w-8 h-8" />
         WorkSphere
       </Link>
     </header>
